965044d00a93c68b2786bba62c3eb3ad8b410acb
taisun93 <taisun93@gmail.com>
2024-07-08 10:46:52 -0500

    total day

--- assessment.py
+++ assessment.py
@@ -1,17 +1,31 @@
 import datetime
 import argparse
 
-def get_weekday_count(startDate, endDate):
-    count = 0
-    #Could be more optimized if I counted out the weeks and added 5 for each but this is cleaner
-    for day in range(int((endDate-startDate).days)+1):
-        if (startDate + datetime.timedelta(day)).weekday() < 5:
-            count+=1
+def get_total_day(year, month, day):
 
-    return count
+    leap_year_count = (year//4) - (year//100) + (year//400)
+
+    year_day_count = 365 * year +leap_year_count
+
+    month_list = [31, 28 + get_leap_year_day(year), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+    month_day_count = 0
+    
+    for m in range(month):
+        month_day_count+=month_list[m]
+        
+    return year_day_count + month_day_count + day
+
+def day_of_week(total_days):
+    # January 1, 1 CE was a Monday (0)
+    return (total_days - 1) % 7
 
 def get_leap_year_day(year):
     if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return 1
+            return 0
         return 1
     return 0
 
