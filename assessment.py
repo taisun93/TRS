68a65e64312e21e239ea642cb0b8c10e518b0775
taisun93 <taisun93@gmail.com>
2024-07-08 11:09:17 -0500

    time for testing

--- assessment.py
+++ assessment.py
@@ -1,24 +1,50 @@
 import datetime
 import argparse
 
+
+def get_weekday_count(startDay, endDay):
+    whole_weeks = (endDay - startDay) // 7
+
+    remainder = (endDay - startDay) % 7
+
+    weekday_count = 0
+    # Count backwards using the last day
+    for day in range(remainder):
+        if day_of_week(endDay-remainder)<5:
+            weekday_count+=1
+
+
+    return whole_weeks * 5 + weekday_count
+
+
 def get_total_day(year, month, day):
 
-    leap_year_count = (year//4) - (year//100) + (year//400)
+    leap_year_count = (year // 4) - (year // 100) + (year // 400)
 
-    year_day_count = 365 * year +leap_year_count
+    year_day_count = 365 * year + leap_year_count
 
-    month_list = [31, 28 + get_leap_year_day(year), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    month_list = [
+        31,
+        28 + get_leap_year_day(year),
+        31,
+        30,
+        31,
+        30,
+        31,
+        31,
+        30,
+        31,
+        30,
+        31,
+    ]
 
     month_day_count = 0
-    
+
     for m in range(month):
-        month_day_count+=month_list[m]
-        
+        month_day_count += month_list[m]
+
     return year_day_count + month_day_count + day
 
-def day_of_week(total_days):
-    # January 1, 1 CE was a Monday (0)
-    return (total_days - 1) % 7
 
 def get_leap_year_day(year):
     if year % 4 == 0:
@@ -30,20 +56,25 @@ def get_leap_year_day(year):
     return 0
 
 
+def day_of_week(total_days):
+    # January 1, 1 CE was a Monday (0)
+    return (total_days - 1) % 7
+
+
 def main():
 
     parser = argparse.ArgumentParser()
-    #Dates are expected in YYYY-MM-DD format
-    parser.add_argument('startDate', type=str)
-    parser.add_argument('endDate', type=str)
+    # Dates are expected in YYYY-MM-DD format
+    parser.add_argument("startDate", type=str)
+    parser.add_argument("endDate", type=str)
 
     args = parser.parse_args()
 
-    start_date = list(map(int, args.startDate.split('-')))
-    end_date = list(map(int, args.endDate.split('-')))
-    
+    start_date = list(map(int, args.startDate.split("-")))
+    end_date = list(map(int, args.endDate.split("-")))
 
-    # print(get_weekday_count(args.startDate, args.endDate))
+    print(get_weekday_count(get_total_day(*start_date), get_total_day(*end_date)))
+
 
 if __name__ == "__main__":
     main()
